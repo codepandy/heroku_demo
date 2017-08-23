a3069a8cc433b163cf6c724ef5b7801f80fd3f6d
wangpeng <Pengpeng.Wang@Honeywell.com>
2017-08-23 17:20:46 +0800

    better

--- app.js
+++ app.js
@@ -1,34 +1,22 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-http.createServer(function (request, response) {
-    var url = require('url').parse(request.url);
-    var filename = url.pathname.substring(1);
-    
-    var type;
-    switch (filename.substring(filename.lastIndexOf('.') + 1)) {
-        case 'html':
-        case 'htm': type = 'text/html; charset=UTF-8'; break;
-        case 'js': type = 'application/javascript; charset=UTF-8'; break;
-        case 'css': type = 'text/css; charset=UTF-8'; break;
-        case 'txt': type = 'text/plain; charset=UTF-8'; break;
-        case 'manifest': type = 'text/cache-manifest; charset=UTF-8'; break;
-        default: type = 'application/octet-stream'; break;
-    }
-    fs.readFile(filename, function (err, content) {
-        if (err) {
-            response.writeHead(404, {
-                'Content-Type': 'text/plain; charset=UTF-8'
-            });
-            response.write(err.message);
-            response.end();
-        } else {
-            response.writeHead(200, { 'Content-Type': type });
-            response.write(content);
-            response.end();
-        }
-    });
-}).listen(process.env.PORT || 5000);
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 console.log('the server is started.');
 
 
